94ff2750226bf125abf80760e80fdb5884b86d81
g-t <g-t@kali.spoofed.no>
2024-09-29 11:07:01 +0200

    Add existing file

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -11,6 +11,9 @@ public class Matriser {
 		skrivUt(matrise1);
 		System.out.println(erLik(matrise, matrise3));
 		skrivUt(multipliser(matrise, matrise3));
+		skrivUt(matrise);
+		System.out.println("speile:");
+		skrivUt(speile(matrise));
 	}
 	// a)
 	public static void skrivUt(int[][] matrise) {
@@ -55,49 +58,29 @@ public class Matriser {
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 		boolean erlik = false;
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < a[i].length; j++) {
-				if (a[i][j] == b[i][j]) {
-					erlik = true;
-				}
-			}
+		if (a == b) {
+			erlik = true;
 		}
 		return erlik;
 	}
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {
-		int lengde = 0;
+		int[][] tabell = new int[matrise.length][matrise.length];
 		for (int i = 0; i < matrise.length; i++) {
-			for (int j = 0; j < matrise[i].length; j++) {
-				lengde++;
+			for (int j = 0; j < matrise[i].length; j++) { 
+				tabell[i][j] = matrise[j][i];
 			}
 		}
-		int[][] tabellNy = new int[matrise.length][lengde];	
-		for (int a = 0; a < matrise.length; a++) {
-			for (int b = 0; b < matrise[a].length; b++) {
-				tabellNy[a][b] = matrise[a][b];
-			}
-		}
-		return tabellNy;
+		return tabell;
 	}
 
 	// f)
 	public static int[][] multipliser(int[][] a, int[][] b) {
-		int la = 0;
-		int lb = 0;
-		for (int i = 0; i < a.length; i++) {
-			la = a[i].length;
-		}
-		for (int j = 0; j < b.length; j++) {
-			lb = b[j].length;
-		}
-		int[][] multiplisert = new int[a.length][lb];
-		if (a.length == b.length && la == lb) {
-			for (int f = 0; f < a.length; f++) {
-				for (int k = 0; k < la; k++) {
-					multiplisert[f][k] = (a[f][k] * b[f][k]);
-				}
+		int[][] multiplisert = new int[a.length][a.length];
+		for (int i = 0; i < multiplisert.length; i++) {
+			for (int j = 0; j < multiplisert[i].length; j++) {
+				multiplisert[i][j] = (a[i][j] * b[i][j]);
 			}
 		}
 		return multiplisert;
